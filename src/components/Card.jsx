82ad0079e6aac57e4153038b2bba9f97177c412f
oguz-yildiz <oguzyildiz2535@gmail.com>
2022-07-04 14:52:00 +0300

    project completed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,11 @@
 import "./Card.scss";
+import phone from "../assets/phone.svg";
+import emails from "../assets/email.svg";
+import location from "../assets/location.svg";
 
 const Card = ({ user }) => {
   const {
+    picture: { medium },
     name: { title, first, last },
     email,
     cell,
@@ -10,7 +14,35 @@ const Card = ({ user }) => {
     registered: { date },
   } = user;
 
-  return <div></div>;
+  return (
+    <div className="content">
+      <div className="image">
+        <img src={medium} alt="img" />
+      </div>
+      <h3>
+        {title}
+        {first}
+        {last}
+      </h3>
+      <div className="svg">
+        <img src={emails} alt="svg" />
+      </div>
+      <h4>{email}</h4>
+      <div className="svg">
+        <img src={phone} alt="svg" />
+      </div>
+      <h4>{cell}</h4>
+      <div className="svg">
+        <img src={location} alt="svg" />
+      </div>
+      <h4>
+        {country}
+        {city}
+      </h4>
+      <h4 className="age">Age: {age}</h4>
+      <h4 className="regdate">Register Date: {date.substring(0, 10)}</h4>
+    </div>
+  );
 };
 
 export default Card;
